988a174bc77223804fa93a8f9b3ac01b3823f9e6
OlyaY <olga.yukhnovets@gmail.com>
2018-02-26 01:49:33 +0300

    additional_5: Implement function check. test 11/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var str;
+  var bracketsConfig;
+  var arr = [];
+
+  for (i = 0; i < arr.lenght; i++) {
+    for (j = 0; j < bracketsConfig; j++) {
+      if (bracketsConfig[j][0] === bracketsConfig[j][1] &&
+        bracketsConfig[j][1] === str[i] || bracketsConfig[j][0] === str[i]) {
+        arr.push(str[i]);
+      } else if (str[i] === arr[arr.length - 1] || str[i] === bracketsConfig[j][1])
+        arr.pop();
+    }
+  }
+  if (arr.length === 0) return true;
 }
