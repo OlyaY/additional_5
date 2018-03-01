63f629a28cfb0517a60fe98f28d35905d68febdb
OlyaY <olga.yukhnovets@gmail.com>
2018-03-01 12:26:46 +0300

    additional_5 redone test 100/100

--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@ module.exports = function check(str, bracketsConfig) {
   // your solution
   var str;
   var bracketsConfig;
-  var arr = [];
+  let arr = [];
+  let strLen = str.length;
+  bracketsConfigLen = bracketsConfig.length;
 
-  for (i = 0; i < arr.lenght; i++) {
-    for (j = 0; j < bracketsConfig; j++) {
-      if (bracketsConfig[j][0] === bracketsConfig[j][1] &&
-        bracketsConfig[j][1] === str[i] || bracketsConfig[j][0] === str[i]) {
+  for (let i = 0; i < strLen; i++) {
+    for (let j = 0; j < bracketsConfigLen; j++) {
+      if (bracketsConfig[j][1] === str[i] && bracketsConfig[j][0] === str[i]) {
+        if (arr.indexOf(str[i]) === -1) {
+          arr.push(str[i]);
+        } else {
+          if (bracketsConfig[j][0] === arr[arr.length - 1]) {
+            arr.pop();
+          } else {
+            return false;
+          }
+        }
+
+      } else if (bracketsConfig[j][0] === str[i]) {
         arr.push(str[i]);
-      } else if (str[i] === arr[arr.length - 1] || str[i] === bracketsConfig[j][1])
-        arr.pop();
+      } else if (bracketsConfig[j][1] === str[i]) {
+        if (arr[arr.length - 1] === bracketsConfig[j][0]) {
+          arr.pop();
+        } else return false;
+      }
     }
   }
-  if (arr.length === 0) return true;
+  if (arr.length === 0) {
+    return true;
+  } else return false;
 }
